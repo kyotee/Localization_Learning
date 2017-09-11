a6851057d55fd02e2983d1f67f60a773f2d5952e
kyletam27 <kyle_tam1@hotmail.com>
2017-09-11 04:10:55 +0000

    investigate if loop is redundant

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
-# include CapybaraHelper
+I18n.available_locales = [:en, :gr]
 
 feature "localization" do
-    scenario "verfies that text has been localized" do         # , js: true syntax requires phantomjs
-        visit '/static_pages/home'
-
-        expect(page).to have_content(I18n.t('welcome.welcomeheader'))   
-        # expect(page).to have_content(I18n.t('welcome.welcomeheader'))
-        # expect(page).to have_content(I18n.t('welcome.about'))
-        # expect(page).to have_content(I18n.t('content.paragraph'))
+    I18n.available_locales.each do |locale|
+    
+        scenario "verfies that text has been localized" do         # , js: true syntax requires phantomjs
+            visit '/static_pages/home'
+    
+            expect(page).to have_content(I18n.t('welcome.welcomeheader'))   
+            # expect(page).to have_content(I18n.t('welcome.welcomeheader'))
+            # expect(page).to have_content(I18n.t('welcome.about'))
+            # expect(page).to have_content(I18n.t('content.paragraph'))
+        end
+    
     end
 end
